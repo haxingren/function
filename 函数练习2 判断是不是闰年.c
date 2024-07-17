efd81770b595be0d7a3322b27716f1fa42df5aa5
haxingren <tanjingyuan17@gmail.com>
2024-07-17 17:06:49 +0800

    Add files via upload

---
+++ 函数练习2 判断是不是闰年.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int is_leap_year(int year)
+{
+	if (((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0)
+	{
+		return 1;
+	}
+	else
+		return 0;
+}
+int main()
+{
+	int year = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (is_leap_year(year))
+		{
+			printf("%d ", year);
+		}
+	}
+	return 0;
+}
